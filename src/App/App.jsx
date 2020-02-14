bd09d13f15b67cc7fa4e17962088c512ea43bcee
Lasertype <david.lasezkay.com>
2020-02-14 14:38:12 -0800

    setting up NOP component; installed modal

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import NumPlayers from "../NumPlayers/NumPlayers";
+import Modal from "@material-ui/core/Modal";
 
 const App = () => {
+  const [numberOfPlayers, setNumberOfPlayers] = useState(null);
+  const [modal, setModal] = useState(true);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to QuestClerk!</h1>
       </header>
-      <NumPlayers />
+      <Modal open={modal}>
+        <NumPlayers
+          setNumberOfPlayers={setNumberOfPlayers}
+          setModal={setModal}
+        />
+      </Modal>
     </div>
   );
 };
