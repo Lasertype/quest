0a8e56992b13ded669e427221e213ca6b009bb33
Lasertype <david.lasezkay@gmail.com>
2020-02-15 15:57:49 -0800

    added modal and it properly sets num of players

--- src/NumPlayers/NumPlayers.jsx
+++ src/NumPlayers/NumPlayers.jsx
@@ -11,16 +11,16 @@ const NumPlayers = ({ setNumberOfPlayers = () => {}, setModal = () => {} }) => {
     <div className="nop-container">
       <h2 className="nop-title">How Many Players?</h2>
       <div className="nop-number-container">
-        <div className="number" onClick={chooseNOP(1)}>
+        <div className="number" onClick={() => {chooseNOP(1)}}>
           1
         </div>
-        <div className="number" onClick={chooseNOP(2)}>
+        <div className="number" onClick={() => {chooseNOP(2)}}>
           2
         </div>
-        <div className="number" onClick={chooseNOP(3)}>
+        <div className="number" onClick={() => {chooseNOP(3)}}>
           3
         </div>
-        <div className="number" onClick={chooseNOP(4)}>
+        <div className="number" onClick={() => {chooseNOP(4)}}>
           4
         </div>
       </div>
